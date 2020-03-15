794f9a37e80644f0a15af1a38d54a37abe79d96e
UndauntedPhish <benrozario2012@gmail.com>
2020-03-15 00:16:30 -0400

    Updated LobbyAPI.java with new API components.

--- src/com/ben/lobbyapi/LobbyAPI.java
+++ src/com/ben/lobbyapi/LobbyAPI.java
@@ -7,11 +7,13 @@ public class LobbyAPI extends JavaPlugin implements Listener
 {
 	public Timer timer;
 	public QuerySender querySender;
+	public GUI gui;
 	
 	@Override
 	public void onEnable()
 	{
 		timer = new Timer();
 		querySender = new QuerySender();
+		gui = new GUI();
 	}
 }
