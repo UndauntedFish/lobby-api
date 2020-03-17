96e20f5bff2ce0522be03b2e451c97c7280bcd72
UndauntedPhish <benrozario2012@gmail.com>
2020-03-17 11:29:59 -0400

    Removed Listener implementation.

--- src/com/ben/lobbyapi/LobbyAPI.java
+++ src/com/ben/lobbyapi/LobbyAPI.java
@@ -1,13 +1,12 @@
 package com.ben.lobbyapi;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.ben.lobbyapi.components.GUI;
 import com.ben.lobbyapi.components.Timer;
 import com.ben.lobbyapi.components.dbSetup;
 
-public class LobbyAPI extends JavaPlugin implements Listener
+public class LobbyAPI extends JavaPlugin
 {
 	public Timer timer;
 	public dbSetup dbSetup;
